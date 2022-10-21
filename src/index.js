ee922044940b4ac812db95e3b8c586363fc79846
Richard Allen <richard.benjamin.allen@gmail.com>
2022-10-21 12:48:23 +0100

    Updated: Added dev notes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const config = {
   id: 'mirador-viewer',
   windows: [
     {
+      // this is where you add your own static json to mimic the iiif manifest, you still need an image and tile server
       manifestId: 'https://gist.githubusercontent.com/BeebBenjamin/ceb7c61c952d47b8dad5849f0d5f7f98/raw/c22e9eeef0fd7b9dc3d10d3c356eaab97f6e9f36/rawl_copperplates_g21.json',
     }
   ],
