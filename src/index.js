234666503f0e52246d858961820eef24f1555a15
Richard Allen <richard.benjamin.allen@gmail.com>
2022-10-25 15:24:43 +0100

    Fixed: define module in Package.json, export from index, iterable

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-export archioxMiradorPlugin from './plugins/archioxMiradorPlugin';
+import _archioxMiradorPlugin from './plugins/archioxMiradorPlugin';
+export { _archioxMiradorPlugin as archioxMiradorPlugin };
--- src/plugins/archioxMiradorPlugin.js
+++ src/plugins/archioxMiradorPlugin.js
@@ -1,11 +1,11 @@
 import LightNormals from './lightNormals';
 import {getCurrentCanvas} from 'mirador/dist/es/src/state/selectors';
 
-export default {
+export default [{
    target: 'OpenSeadragonViewer',
    mapStateToProps: (state, {windowId}) => ({
         canvas: getCurrentCanvas(state, { windowId })
     }),
     mode: 'add',
     component: LightNormals,
-}
+}]
