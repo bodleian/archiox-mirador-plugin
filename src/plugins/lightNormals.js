c3594043b5a52d672ac07a09c15bbfdef373e480
Richard Allen <richard.benjamin.allen@gmail.com>
2022-11-03 12:37:14 +0000

    Fixed: Get service image by type not index position

--- src/plugins/lightNormals.js
+++ src/plugins/lightNormals.js
@@ -53,7 +53,11 @@ function getMap(annotationBodies, mapType) {
         const service = element.getService('http://iiif.io/api/annex/services/lightingmap');
         if (service !== null) {
             if(service.__jsonld.mapType === mapType){
-                map = element.__jsonld.service[1]['id'];
+                element.__jsonld.service.forEach(function(service){
+                    if(service.type === "ImageService3") {
+                        map = service['id'];
+                    }
+                });
             }
         }
     });
