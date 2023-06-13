320f9d2bfa0b0606858ec2468da30ede5b3df4cf
Richard Benjamin Allen <richard.allen@bodleian.ox.ac.uk>
2023-06-13 11:18:44 +0100

    Fixed: Moved and refactored ThreeOverlay component

--- src/plugins/RelightThreeOverlay.js
+++ src/plugins/RelightThreeOverlay.js
@@ -1,40 +1,56 @@
-import RelightThreeCanvas from "./RelightThreeCanvas";
-import React, { Component } from "react";
+import RelightThreeCanvas from './RelightThreeCanvas';
+import React, { Component } from 'react';
 
 import * as THREE from 'three';
 
-import PropTypes from 'prop-types';
+import PropTypes, { shape } from 'prop-types';
 class RelightThreeOverlay extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render () {
-        const { images, zoom, rendererInstructions, contentWidth, contentHeight, lightX, lightY, directionalIntensity,ambientIntensity, tileLevel, minTileLevel, maxTileLevel, tileSets, tileLevels } = this.props;
-        return (
-            <RelightThreeCanvas
-                images={images}
-                zoom={zoom}
-                intersection={rendererInstructions.intersection}
-                contentWidth={contentWidth}
-                contentHeight={contentHeight}
-                lightX={lightX}
-                lightY={lightY}
-                directionalIntensity={directionalIntensity}
-                ambientIntensity={ambientIntensity}
-                tileLevel={tileLevel}
-                minTileLevel={minTileLevel}
-                maxTileLevel={maxTileLevel}
-                tileSets={tileSets}
-                tileLevels={tileLevels}
-            />
-        );
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const {
+      images,
+      zoom,
+      rendererInstructions,
+      contentWidth,
+      contentHeight,
+      lightX,
+      lightY,
+      directionalIntensity,
+      ambientIntensity,
+      tileLevel,
+      minTileLevel,
+      maxTileLevel,
+      tileSets,
+      tileLevels,
+    } = this.props.threeCanvasProps;
+    return (
+      <RelightThreeCanvas
+        images={images}
+        zoom={zoom}
+        rendererInstructions={rendererInstructions.intersection}
+        contentWidth={contentWidth}
+        contentHeight={contentHeight}
+        lightX={lightX}
+        lightY={lightY}
+        directionalIntensity={directionalIntensity}
+        ambientIntensity={ambientIntensity}
+        tileLevel={tileLevel}
+        minTileLevel={minTileLevel}
+        maxTileLevel={maxTileLevel}
+        tileSets={tileSets}
+        tileLevels={tileLevels}
+      />
+    );
+  }
 }
 
 RelightThreeOverlay.propTypes = {
+  threeCanvasProps: shape({
     images: PropTypes.arrayOf(THREE.Texture.type).isRequired,
     zoom: PropTypes.number.isRequired,
-    intersection: PropTypes.number.isRequired,
+    rendererInstructions: PropTypes.number.isRequired,
     contentWidth: PropTypes.number.isRequired,
     contentHeight: PropTypes.number.isRequired,
     lightX: PropTypes.number.isRequired,
@@ -45,6 +61,7 @@ RelightThreeOverlay.propTypes = {
     minTileLevel: PropTypes.number.isRequired,
     maxTileLevel: PropTypes.number.isRequired,
     tileSets: PropTypes.arrayOf(PropTypes.any).isRequired, // might need to go into this in more detail as to what this is composed of
-    tileLevels: PropTypes.arrayOf(PropTypes.number).isRequired
-}
+    tileLevels: PropTypes.arrayOf(PropTypes.number).isRequired,
+  }),
+};
 export default RelightThreeOverlay;
