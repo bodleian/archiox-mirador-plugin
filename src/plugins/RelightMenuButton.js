a7f83945888dbd7edf4062f23648864fc650e536
Richard Benjamin Allen <richard.allen@bodleian.ox.ac.uk>
2023-06-13 12:34:38 +0100

    Fixed: Moved and refactored MenuButton component

---
+++ src/plugins/RelightMenuButton.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+import HighlightIcon from '@material-ui/icons/Highlight';
+import CloseSharpIcon from '@material-ui/icons/CloseSharp';
+import { MiradorMenuButton } from 'mirador/dist/es/src/components/MiradorMenuButton';
+import PropTypes from 'prop-types';
+
+class RelightMenuButton extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const { open, onClick } = this.props;
+    return (
+      <MiradorMenuButton
+        aria-label={
+          open ? 'Collapse Relighting Tools' : 'Expand Relighting Tools'
+        }
+        style={{
+          float: 'left',
+          clear: 'both',
+        }}
+        onClick={onClick}
+      >
+        {open ? <CloseSharpIcon /> : <HighlightIcon />}
+      </MiradorMenuButton>
+    );
+  }
+}
+
+RelightMenuButton.propTypes = {
+  open: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+export default RelightMenuButton;
