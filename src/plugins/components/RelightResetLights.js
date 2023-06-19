b3c019be5dfd2284fec07fd510e8743a37aa3565
Richard Benjamin Allen <richard.allen@bodleian.ox.ac.uk>
2023-06-19 15:19:13 +0100

    Fixed: Added doc strings

--- src/plugins/components/RelightResetLights.js
+++ src/plugins/components/RelightResetLights.js
@@ -3,6 +3,10 @@ import ReplaySharpIcon from '@material-ui/icons/ReplaySharp';
 import PropTypes from 'prop-types';
 import { MiradorMenuButton } from 'mirador/dist/es/src/components/MiradorMenuButton';
 
+/**
+ * The RelightResetLights component is a plug-in button that will reset the state and appearance of the light controls
+ * to their default values when it is clicked.
+ */
 class RelightResetLights extends React.Component {
   constructor(props) {
     super(props);
@@ -26,6 +30,7 @@ class RelightResetLights extends React.Component {
 }
 
 RelightResetLights.propTypes = {
+  /** The onClick prop is a function used to manage component behaviour when the component is clicked **/
   onClick: PropTypes.func.isRequired,
 };
 
