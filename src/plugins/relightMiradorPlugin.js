7860aaf46d84701c3a72769f28aeb4c21cd5f198
Richard Benjamin Allen <richard@palaeopi.org.uk>
2023-11-16 21:51:57 +0000

    Fixed: Basic example of redux-saga working

--- src/plugins/relightMiradorPlugin.js
+++ src/plugins/relightMiradorPlugin.js
@@ -1,11 +1,32 @@
 import Relight from './Relight';
 import { getCurrentCanvas } from 'mirador/dist/es/src/state/selectors';
 import { getWindow } from 'mirador/dist/es/src/state/selectors';
-
 import * as actions from 'mirador/dist/es/src/state/actions';
+import ActionTypes from "mirador/dist/es/src/state/actions/action-types";
+import { takeEvery } from 'redux-saga/effects';
+
+function resourceAdded(action) {
+  if (action.type === ActionTypes.ADD_WINDOW) {
+    try { console.log("Resource Added"); } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+// Worker saga will be fired on ADD_RESOURCE and ADD_WINDOW action
+function* rootSaga() {
+  yield takeEvery(
+      [
+        ActionTypes.ADD_RESOURCE,
+        ActionTypes.ADD_WINDOW,
+      ],
+      resourceAdded
+  )
+}
 
 export default [
   {
+    saga: rootSaga,
     target: 'OpenSeadragonViewer',
     mapDispatchToProps: {
       updateLayers: actions.updateLayers,
