2df1ccfced7e924bd4a2c17898639578b8e638cd
Richard Benjamin Allen <richard@palaeopi.org.uk>
2023-11-22 16:07:19 +0000

    Fixed: Saga methods need yield to work with Mirador
    Mirador selectors and actions need to be yielded in generators otherwise nothing happens.  I had to duplicate the functionality of updateLayer to get it to work.

--- src/plugins/state/sagas.js
+++ src/plugins/state/sagas.js
@@ -1,24 +1,36 @@
-import { getCanvases } from 'mirador/dist/es/src/state/selectors';
-
+import { getCanvases, getWindows } from 'mirador/dist/es/src/state/selectors';
 import ActionTypes from 'mirador/dist/es/src/state/actions/action-types';
-import { takeEvery, select } from 'redux-saga/effects';
-import { updateLayer, getLayers } from '../RelightHelpers';
+import { put, select, takeEvery } from 'redux-saga/effects';
+import { getLayers } from '../RelightHelpers';
 import * as actions from 'mirador/dist/es/src/state/actions';
 
 export function* setCanvas(action) {
+  const updateLayers = actions.updateLayers;
   const windowId = action.windowId;
-  const canvasId = action.canvasId;
-  const canvas = yield select(getCanvases, { windowId });
-  const layers = getLayers(canvas[0].iiifImageResources);
   const excluded_maps = ['composite'];
-  updateLayer(
-    windowId,
-    actions.updateLayers,
-    excluded_maps,
-    canvasId,
-    layers,
-    true
-  );
+  const windows = yield select(getWindows, windowId);
+  const windowIds = Object.keys(windows).map((item) => {
+    return windows[item].id;
+  });
+
+  for (let windowId of windowIds) {
+    const canvas = yield select(getCanvases, { windowId });
+    const layers = getLayers(canvas[0].iiifImageResources);
+    const layer_keys = Object.keys(layers).map((item) => {
+      return item;
+    });
+
+    for (let layer of layer_keys) {
+      const mapType = layers[layer].trim();
+
+      if (excluded_maps.includes(mapType)) {
+        const payload = {
+          [layer]: { visibility: true },
+        };
+        yield put(updateLayers(windowId, canvas[0].id, payload));
+      }
+    }
+  }
 }
 
 export function* rootSaga() {
