b8f2d1c736f1a34dc68488a5490000ee52284f05
Richard Benjamin Allen <richard@palaeopi.org.uk>
2024-01-18 21:01:56 +0000

    Fixed: remove deprecated image, add setImageFormatsSupported

--- src/plugins/Relight.js
+++ src/plugins/Relight.js
@@ -404,21 +404,23 @@ class Relight extends React.Component {
           false
         );
 
+        this.props.viewer.setImageFormatsSupported({webp: true})
+
         // add an event handler to build Three textures from the tiles as they are loaded, this means they can be
         // reused and sent to the Three canvas.
         this.props.viewer.addHandler('tile-loaded', (event) => {
           this.setState({ loadHandlerAdded: true });
           this.tileLevels[event.tile.level] = event.tile.level;
           this.tileLevel = event.tile.level;
-          const sourceKey = event.image.currentSrc.split('/')[5];
+          const sourceKey = event.data.currentSrc.split('/')[5];
           const canvas = document.createElement('canvas');
-          const tileTexture = new THREE.Texture(event.image);
+          const tileTexture = new THREE.Texture(event.data);
           const key = event.tile.cacheKey;
 
-          canvas.width = event.image.width;
-          canvas.height = event.image.height;
+          canvas.width = event.data.width;
+          canvas.height = event.data.height;
           event.tile.context2D = canvas.getContext('2d');
-          event.tile.context2D.drawImage(event.image, 0, 0);
+          event.tile.context2D.drawImage(event.data, 0, 0);
 
           tileTexture.needsUpdate = true;
 
